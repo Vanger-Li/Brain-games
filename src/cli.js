2995b1acd132dbfbacead3d9fed2785cd9ef43c0
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-18 20:00:22 +0500

    refactoring step two

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
+  console.log('Welcome to the Brain games');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 };
