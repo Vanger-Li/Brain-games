2def48e8ac163a84e1b92bbbd02f7df6fdefec8b
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-19 18:52:28 +0500

    updated function.js

--- src/function.js
+++ src/function.js
@@ -1,3 +1,5 @@
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export default getRandomInteger;
+
+export const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
