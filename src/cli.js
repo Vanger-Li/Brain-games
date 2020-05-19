661c1e77b0ef6492a8bffc03f4751f693bcbc3bc
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-19 18:51:37 +0500

    updated cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import getRandomInteger from './function';
 
 const greeting = () => {
   console.log('Welcome to the Brain games');
@@ -7,6 +8,8 @@ const greeting = () => {
 };
 
 export const instructionsForGame = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+export const getQuestion = () => getRandomInteger(1, 100);
 
 export const getAnswerFromUser = () => readlineSync.question('Your answer: ');
 
