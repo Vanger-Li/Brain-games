0b4e128bb8e26fbc82aedadf62c2b49508d37658
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-19 15:06:57 +0500

    updated cli.js

--- src/cli.js
+++ src/cli.js
@@ -5,4 +5,9 @@ const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 };
+
+export const instructionsForGame = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+export const getAnswerFromUser = () => readlineSync.question('Your answer: ');
+
 export default greeting;
