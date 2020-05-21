b1cc44fc8aeed9781ed5b84e2cc434192464023d
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-21 16:24:59 +0500

    transfer getRandomInteger in function.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,10 @@
 import { gameEngin } from '../index.js';
+import getRandomInteger from '../function.js';
 
-const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 const instructionsForGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const question = `Number ${getRandomInteger(1, 100)} is even?`;
+const question = getRandomInteger(1, 100);
 
 const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
