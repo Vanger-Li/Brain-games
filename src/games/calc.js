0dcb258852df38dce4ef0521cf1ab032e525e78c
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-21 17:28:45 +0500

    updated calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,33 @@
+import { gameEngin } from '../index.js';
+import getRandomInteger from '../function.js';
+
+const instructionsForGame = 'What is the result of the expression?';
+
+const operators = ['+', '-', '*'];
+
+const calculate = (firstOperand, secondOperand, operator) => {
+  switch (operator) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    default:
+      return false;
+  }
+};
+
+
+const getGameData = () => {
+  const operator = operators[getRandomInteger(0, 2)];
+  const firstOperand = getRandomInteger(0, 50);
+  const secondOperand = getRandomInteger(0, 50);
+  const question = `"What is the result of the expression: ${firstOperand} ${operator} ${secondOperand}"`;
+  const trueAnswer = calculate(firstOperand, secondOperand, operator);
+  return [question, trueAnswer];
+};
+
+const brainCalc = () => gameEngin(instructionsForGame, getGameData);
+
+export default brainCalc;
