92b67c16f633340e49f8ff77cf02f7c3169b3ea8
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-21 15:40:35 +0500

    create the game Engin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import readlineSync from 'readline-sync';
 
-let name;
+let userName;
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain games');
-  name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
 };
 
-export const getUserName = () => name;
+const gameEngin = (instructionsForGame, question, trueAnswer) => {
+  console.log('Welcome to the Brain games');
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+
+  console.log(instructionsForGame);
+
+  const roundsCount = 3;
+  for (let round = 1; round <= roundsCount; round += 1) {
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== trueAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${trueAnswer}".`);
+      console.log(`Let's train again ${userName}`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations! ${userName}`);
+};
+
+export { greeting, gameEngin };
