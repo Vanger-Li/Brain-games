64b2e960b4ddb98f8de3415c95e45722bb818a5b
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-22 17:37:54 +0500

    updated progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,3 +2,28 @@ import { gameEngin } from '../index.js';
 import getRandomInteger from '../function.js';
 
 const instructionsForGame = 'What number is missing in the progression?';
+
+const getProgression = () => {
+  const firstMember = getRandomInteger(0, 50);
+  const stepOfProgression = getRandomInteger(0, 20);
+  const progressionLength = 10;
+  const progression = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(firstMember + stepOfProgression * i);
+  }
+  return progression;
+};
+
+
+const getGameData = () => {
+  const progression = getProgression();
+  const randomIndex = getRandomInteger(0, 9);
+  const trueAnswer = String(progression[randomIndex]);
+  progression[randomIndex] = '..';
+  const question = `"What number is missing in: ${progression}?"`;
+  return [question, trueAnswer];
+};
+
+const brainProgression = () => gameEngin(instructionsForGame, getGameData);
+
+export default brainProgression;
