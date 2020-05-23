b34be0e2adce846d2bd9a3197a67fd80b873ad13
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-23 13:12:25 +0500

    created prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,5 @@
+import { gameEngin } from '../index.js';
+import getRandomInteger from '../function.js';
+
+
+const instructionsForGame = 'Answer "yes" if the number is prime, otherwise answer "no".';
