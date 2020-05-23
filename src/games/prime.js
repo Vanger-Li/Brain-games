e226c6e7ebefaa0e62ccaad4c924b1b7a45e350c
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-23 13:27:49 +0500

    updated prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,3 +3,20 @@ import getRandomInteger from '../function.js';
 
 
 const instructionsForGame = 'Answer "yes" if the number is prime, otherwise answer "no".';
+
+const isPrime = (num) => {
+
+} 
+
+
+const getGameData = () => {
+  const integer = getRandomInteger(1, 100);
+  const question = `"Is the number ${integer} even?"`;
+  const trueAnswer = isPrime(integer);
+  return [question, trueAnswer];
+};
+
+
+const brainPrime = () => gameEngin(instructionsForGame, getGameData);
+
+export default brainPrime;
