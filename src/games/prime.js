d8e022f125896ad78294553f1dd286287b7a6b63
vanger-li <vitaliy.vanger32@gmail.com>
2020-05-23 13:39:46 +0500

    updated prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,14 +4,21 @@ import getRandomInteger from '../function.js';
 
 const instructionsForGame = 'Answer "yes" if the number is prime, otherwise answer "no".';
 
-const isPrime = (num) => {
-
-} 
+const isPrime = (number) => {
+  let counter = 2;
+  while (counter <= number) {
+    if (number % counter === 0 && number !== counter) {
+      return 'no';
+    }
+    counter += 1;
+  }
+  return 'yes';
+};
 
 
 const getGameData = () => {
   const integer = getRandomInteger(1, 100);
-  const question = `"Is the number ${integer} even?"`;
+  const question = `"Is the number ${integer} prime?"`;
   const trueAnswer = isPrime(integer);
   return [question, trueAnswer];
 };
