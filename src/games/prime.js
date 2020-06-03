367dc1c47671f287849a66c7b2d912334c1b750c
vanger-li <vitaliy.vanger32@gmail.com>
2020-06-03 12:34:57 +0500

    code refactoring

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,14 +1,13 @@
 import { runGameEngine } from '../index.js';
 import { generateRandomNumber } from '../utils.js';
 
-
 const gameDescription = 'Answer "yes" if the number is prime, otherwise answer "no".';
 
 const isPrime = (number) => {
-  let counter = 2;
   if (number <= 1) {
     return false;
   }
+  let counter = 2;
   while (counter <= number / 2) {
     if (number % counter === 0) {
       return false;
@@ -18,14 +17,12 @@ const isPrime = (number) => {
   return true;
 };
 
-
 const generateRoundData = () => {
   const number = generateRandomNumber(1, 100);
   const question = `${number}`;
   const correctAnswer = (isPrime(number) ? 'yes' : 'no');
   return [question, correctAnswer];
 };
-
 
 const runPrimeGame = () => runGameEngine(gameDescription, generateRoundData);
 
